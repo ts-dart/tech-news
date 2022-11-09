6451f93ca0033665834e2bc46f91fe138a7dab25
thiago <thiagoedusan5.11@outlook.com>
2022-11-09 20:54:25 -0300

    ...

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from time import sleep
 from parsel import Selector
@@ -23,10 +24,34 @@ def scrape_next_page_link(html_content):
     return selector.css('a.next ::attr(href)').get()
 
 
-# Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    title = selector.css('h1.entry-title ::text').get()
 
+    news = {
+        'url': get_url(title, selector.css('link ::attr(href)').getall()),
+        'title': selector.css('h1.entry-title ::text').get(),
+        'writer': selector.css('span.author a ::text').get(),
+        'summary': (
+            re.sub('<.*?>', '', selector.css('p').get()).replace(u'\xa0', u'')
+        ),
+        'comments_count': len(selector.css('ol.comment-list li').getall()),
+        'timestamp': selector.css('li.meta-date ::text').get(),
+        'tags': selector.css('section.post-tags li a ::text').getall(),
+        'category': selector.css('span.label ::text').get(),
+    }
+
+    ''' print('-----------------')
+    print(news)
+    print('-----------------')
+    print(news['summary']) '''
+    return news
+
+
+def get_url():
+    ...
+
+    # selector.css('link ::attr(href)').getall()[3][:-len('amp/')]
 
 # Requisito 5
 def get_tech_news(amount):
