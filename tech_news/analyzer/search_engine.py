745463e995458bfe3f568cfb2c7ee4c6262f4aef
thiago <thiagoedusan5.11@outlook.com>
2022-11-11 17:25:33 -0300

    feat: Foram desenvolvidas funções de filtragem no arquivo search_engine

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,53 @@
-# Requisito 6
+from datetime import datetime
+from tech_news.database import search_news
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_list = search_news({})
+    news_found = []
+
+    for news in news_list:
+        if title.lower() in news['title']:
+            news_found.append((news['title'], news['url']))
+
+    return news_found
 
 
-# Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        news_list = search_news({})
+        date_arr = date.split('-')
+        formated_date = f'{date_arr[2]}/{date_arr[1]}/{date_arr[0]}'
+        datetime.strptime(formated_date, '%d/%m/%Y')
+        news_found = []
+
+        for news in news_list:
+            if formated_date == news['timestamp']:
+                news_found.append((news['title'], news['url']))
+
+        return news_found
+    except (ValueError, IndexError):
+        raise ValueError('Data inválida')
 
 
-# Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    news_list = search_news({})
+    news_found = []
+
+    for news in news_list:
+        for tg in news['tags']:
+            if tag.lower() == tg.lower():
+                news_found.append((news['title'], news['url']))
+
+    return news_found
 
 
-# Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news_list = search_news({})
+    news_found = []
+
+    for news in news_list:
+        if category.lower() == news['category'].lower():
+            news_found.append((news['title'], news['url']))
+
+    return news_found
